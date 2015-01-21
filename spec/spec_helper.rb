dcb7464374ce4ec80708ed23566036ca7f38af97
PJ Kelly <pj@crushlovely.com>
2015-01-20 22:50:36 -0800

    Configured CodeClimate.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,8 @@
 require 'rubygems'
 
+require 'codeclimate-test-reporter'
+CodeClimate::TestReporter.start
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'production'
 
