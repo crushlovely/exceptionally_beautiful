8d7708730d7c0b69e640a40280e2f3b67a80da7a
PJ Kelly <pj@crushlovely.com>
2015-01-21 22:38:39 -0800

    Switched to using numeric representation of status codes in translation files.
    This is instead of Rack's weird, underscored string representations. The gotcha here is that the keys must be prefixed with a : to symbolize it, thus allowing it to work with I18n's lookup system.
    
    Updated Readme accordingly.

--- app/models/exceptionally_beautiful/error.rb
+++ app/models/exceptionally_beautiful/error.rb
@@ -1,10 +1,9 @@
 module ExceptionallyBeautiful
   class Error
-    attr_reader :status_code, :status
+    attr_reader :status_code
 
     def initialize(status_code)
       @status_code = status_code.to_i
-      determine_status
     end
 
     def title
@@ -17,25 +16,19 @@ module ExceptionallyBeautiful
 
     private
 
-    def determine_status
-      match = Rack::Utils::SYMBOL_TO_STATUS_CODE.detect { |k, v| v == status_code }
-      if match
-        @status = match.first
-      else
-        log('The status code passed in is not recognized by Rack.')
-        @status = 'default'
-      end
-    end
-
     def pretty_error_translation(key)
-      I18n.t(key, :scope => ExceptionallyBeautiful.translation_scope(status), :raise => true)
+      I18n.t(key, :scope => translation_scope, :raise => true)
     rescue I18n::MissingTranslationData
-      log("Translation missing: #{ExceptionallyBeautiful.translation_scope(status)}.#{key}. Falling back to defaults.")
+      log("Translation missing: #{translation_scope}.#{key}. Falling back to defaults.")
       I18n.t(key, :scope => ExceptionallyBeautiful.translation_scope('default'))
     end
 
     def log(message)
       ExceptionallyBeautiful.log(message)
+    end
+
+    def translation_scope
+      ExceptionallyBeautiful.translation_scope(status_code)
     end
   end
 end
