4296783ce4b587ed73b5f3b951a49e1f79d95b35
Jill Pittman Adams <jill@crushlovely.com>
2015-01-22 07:37:19 -0800

    Updated error messages and specs.
    Co-authored w/ @pjkelly.

--- spec/features/exceptionally_beautiful_engine_spec.rb
+++ spec/features/exceptionally_beautiful_engine_spec.rb
@@ -13,9 +13,21 @@ feature 'something' do
 
   describe 'default errors' do
     [403, 404, 422, 500, 502].each do |error_code|
-      it "responds with translation content for #{error_code}" do
-        visit "/errors/#{error_code}"
-        expect(page).to have_content(ExceptionallyBeautiful::Error.new(error_code).message)
+      describe "#{error_code} error messaging" do
+        let(:error) { ExceptionallyBeautiful::Error.new(error_code) }
+        before { visit "/errors/#{error_code}" }
+
+        it 'responds with the correct status code' do
+          expect(page.status_code).to eq(error_code)
+        end
+
+        it 'includes the error title' do
+          expect(page).to have_content(error.title)
+        end
+
+        it 'includes the error message rendered in Markdown' do
+          expect(page.html).to include(ExceptionallyBeautiful::MarkdownRenderer.new(error.message).render)
+        end
       end
     end
   end
