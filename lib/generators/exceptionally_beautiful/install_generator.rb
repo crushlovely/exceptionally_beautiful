f19536fa0f1441c2108de039a77f75fe55878fe3
PJ Kelly <pj@crushlovely.com>
2015-01-21 21:47:01 -0800

    Fixed some syntax inconsistencies.

--- lib/generators/exceptionally_beautiful/install_generator.rb
+++ lib/generators/exceptionally_beautiful/install_generator.rb
@@ -4,18 +4,18 @@ require 'securerandom'
 module ExceptionallyBeautiful
   module Generators
     class InstallGenerator < Rails::Generators::Base
-      source_root File.expand_path("../../templates", __FILE__)
+      source_root File.expand_path('../../templates', __FILE__)
 
       def copy_initializer
-        template "exceptionally_beautiful.rb", "config/initializers/exceptionally_beautiful.rb"
+        template 'exceptionally_beautiful.rb', 'config/initializers/exceptionally_beautiful.rb'
       end
 
       def copy_locale
-        copy_file "../../../config/locales/en.yml", "config/locales/exceptionally_beautiful.en.yml"
+        copy_file '../../../config/locales/en.yml', 'config/locales/exceptionally_beautiful.en.yml'
       end
 
       def add_exceptionally_beautiful_route
-        route "exceptionally_beautiful_routes"
+        route 'exceptionally_beautiful_routes'
       end
     end
   end
